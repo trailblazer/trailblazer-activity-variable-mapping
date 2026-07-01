d746014203a1768927d2e7762803536f73ebd143
Nick Sutterer <apotonick@gmail.com>
2026-07-01 16:42:46 +0200

    test the shit outta Inject.

--- test/dsl_test.rb
+++ test/dsl_test.rb
@@ -38,15 +38,82 @@ class DslTest < Minitest::Spec
 end
 
 class DslIntegrationTest < Minitest::Spec
-  it "single Inject(): create private context, no vars added or exposed" do
-    assert_dsl Trailblazer::Activity::VariableMapping::DSL::Inject() => [:http],
-      expected: {captured: ["{}", "{}"]}
+  let(:dsl) { Trailblazer::Activity::VariableMapping::DSL }
+
+  describe "#Inject" do
+    let(:options) { {dsl::Inject() => [:http]} }
+
+    it "with empty ctx, nothing gets injected" do
+      assert_dsl **options, expected: {captured: ["{}", "{}"]}
+    end
+
+    it "with variables in ctx, those are visible" do
+      assert_dsl **options, expected: {captured: ["{:random=>1}", "{:random=>1}"]}, target_ctx: {random: 1}
+    end
+
+    it "with variables in ctx + inject variable, all are visible" do
+      assert_dsl **options, expected: {captured: ["{:random=>1, :http=>2}", "{:random=>1, :http=>2}"]}, target_ctx: {random: 1, http: 2}
+    end
+
+    it "injected variable can be {nil}" do
+      assert_dsl **options, expected: {captured: ["{:random=>1, :http=>nil}", "{:random=>1, :http=>nil}"]}, target_ctx: {random: 1, http: nil}
+    end
+
+    it "without Out(), {pollute} is visible outside" do
+      assert_dsl **options,
+        expected: {
+          captured: ["{:pollute=>true}", "{}"],
+          pollute: 1 # outside, we see the pollution.
+        },
+        target_ctx: {pollute: true}
+    end
+
+    it "without Out() + inject variable, {pollute} is visible outside" do
+      assert_dsl **options,
+        expected: {
+          captured: ["{:pollute=>true, :http=>Object}", "{:http=>Object}"],
+          pollute: 1 # outside, we see the pollution.
+        },
+        target_ctx: {pollute: true, http: Object}
+    end
+
+    it "we can have multiple Inject with one configured variable" do
+      options = {
+        **self.options,
+        dsl::Inject() => [:db],
+        dsl::Inject() => [:logger],
+      }
+
+      assert_dsl **options,
+        expected: {captured: ["{:random=>1, :http=>2, :logger=>Object}", "{:random=>1, :http=>2, :logger=>Object}"]}, target_ctx: {random: 1, http: 2, logger: Object}
+
+      # test that :db is also injected.
+      assert_dsl **options,
+        expected: {captured: ["{:random=>1, :db=>Object}", "{:random=>1, :db=>Object}"]}, target_ctx: {random: 1, db: Object}
+    end
+
+    it "we can also configure multiple variables" do
+      options = {
+        **self.options,
+        dsl::Inject() => [:db, :logger]
+      }
+
+      assert_dsl **options,
+        expected: {captured: ["{:random=>1, :http=>2, :logger=>Object}", "{:random=>1, :http=>2, :logger=>Object}"]}, target_ctx: {random: 1, http: 2, logger: Object}
+
+      # test that :db is also injected.
+      assert_dsl **options,
+        expected: {captured: ["{:random=>1, :db=>Object}", "{:random=>1, :db=>Object}"]}, target_ctx: {random: 1, db: Object}
+    end
   end
 
-  def my_capture_step(ctx, pollute: false, **kws)
-    ctx[:pollute] = 1 if pollute
 
+
+  def my_capture_step(ctx, pollute: false, **kws)
     ctx[:captured] = [CU.inspect(ctx.to_h), CU.inspect(kws)]
+
+    ctx[:pollute] = 1 if pollute
+    true
   end
 
   def mock_task_wrap_circuit(adds_for_task_wrap:, call_task: method(:my_capture_step), **)
